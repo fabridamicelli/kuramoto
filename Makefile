0a1f8c4a51158b42636b6544de20482f4e0161ab
Fabrizio Damicelli <f.damicelli@uke.de>
2021-03-27 20:00:51 +0100

    make package

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
 test:
-	pytest tests.py
+	pytest tests/test_initialization.py
+
+release:
+	rm dist/*
+	python setup.py sdist bdist_wheel
+	twine upload dist/*
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+
+DESCRIPTION = 'Kuramoto model on graphs'
+
+base_packages = [
+    "numpy>=1.16.0",
+]
+
+test_packages = [
+    "pytest",
+    "mypy",
+]
+
+
+setup(
+    name='kuramoto',
+    version='0.1',
+    description=DESCRIPTION,
+    author='Fabrizio Damicelli',
+    author_email='fabridamicelli@gmail.com',
+    url="https://github.com/fabridamicelli/kuramoto",
+    packages=find_packages(exclude=['notebooks', 'docs']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+    ],
+    python_requires='>=3.6',
+    install_requires=base_packages,
+    include_package_data=True
+)
